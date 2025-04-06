353813dd691d1c0eca17aafccbc421f1c8a2bcee
James Forward <james@forwardeder.com>
2025-04-06 16:31:43 +0100

    fix: Delete unnecessary files

--- src/native-test/kotlin/com/virtuallyapp/MyKotlinEntityResourceIT.kt
+++ src/native-test/kotlin/com/virtuallyapp/MyKotlinEntityResourceIT.kt
@@ -3,4 +3,4 @@ package com.virtuallyapp
 import io.quarkus.test.junit.QuarkusIntegrationTest
 
 @QuarkusIntegrationTest
-class ExampleResourceIT : ExampleResourceTest()
+class MyKotlinEntityResourceIT : MyKotlinEntityResourceTest()
